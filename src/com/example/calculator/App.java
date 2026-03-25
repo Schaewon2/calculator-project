68bcdfb39895242b5cb6f355123d074c6e29ac4a
Schaewon2 <schaewon1206@gmail.com>
2026-03-25 20:23:45 +0900

    refactor: 미완성본

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -6,55 +6,18 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        ArithmeticCalculator<Double> calculator = new ArithmeticCalculator<>();
-        OperatorType op;
-
         Scanner sc = new Scanner(System.in);
+        ArithmeticCalculator<Double> calculator = new ArithmeticCalculator<>();
 
         while (true) {
-            // 첫 번째, 두 번째 숫자 입력
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            double num1 = sc.nextDouble();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            double num2 = sc.nextDouble();
+            calculator.inputNumber();
+            calculator.inputOperator();
 
-            // 사칙연산 기호 입력 후에 char 타입에서 enum 타입으로 형변환
-            try {
-                System.out.print("사칙연산 기호를 입력하세요: ");
-                op = OperatorType.fromOperator(sc.next().charAt(0));
-            } catch (IllegalArgumentException e) {
-                String message = e.getMessage();
-                System.out.println(message + " 다시 입력해주세요.");
-                continue;
-            }
+            double result = calculator.calculate(calculator.getNum1(), calculator.getNum2(), calculator.getOp());
 
-            // 사칙연산 메서드 실행하여 result 변수에 담아줌.
-            double result = calculator.calculate(num1, num2, op);
+            calculator.addResult(result); // 리스트에 계산한 결과 추가
 
-            // result를 calculator 객체의 리스트에 추가
-            calculator.addResult(result);
-            System.out.println("계산 결과: " + result);
-            System.out.println("현재 저장된 결과: " + calculator.getResults());
-
-            // 가장 오래된 계산 기록 삭제
-            System.out.println("가장 오래된 계산 기록을 삭제하시겠습니까? (yes 입력 시 삭제)");
-            if (sc.next().equals("yes")) {
-                calculator.removeResult();
-            }
-
-            // 저장된 연산 결과들 중 Scanner로 입력받은 값보다 큰 결과값들을 출력
-            System.out.println("저장된 연산 결과들 중 입력 받은 값보다 큰 결과값들을 출력하시겠습니까? (yes 입력 시 출력)");
-            if (sc.next().equals("yes")) {
-                List<Double> resultGreaterThan = calculator.getResultGreaterThan(num1, num2);
-                System.out.println("저장된 연산 결과들 중 입력 받은 값보다 큰 결과값: " + resultGreaterThan);
-            }
-
-            // 더 계산할지 여부 체크 (exit 입력 시 반복문을 즉시 빠져나오고 계산 종료)
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            if (sc.next().equals("exit")) {
-                System.out.println("계산을 종료합니다.");
-                break;
-            }
+            calculator.optionPrint(); // 목록 출력
         }
     }
 }
--- src/com/example/calculator/ArithmeticCalculator.java
+++ src/com/example/calculator/ArithmeticCalculator.java
@@ -3,24 +3,44 @@ package com.example.calculator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public class ArithmeticCalculator<T extends Number> {
+    Scanner scanner = new Scanner(System.in);
+    // 계산 결과를 담을 리스트 생성
     private List<Double> results = new ArrayList<>();
+    private double num1;
+    private double num2;
+    OperatorType op;
 
+    public void inputNumber(){
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        num1 = scanner.nextDouble();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        num2 = scanner.nextDouble();
+    }
+
+    public void inputOperator() {
+        try {
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            op = OperatorType.fromOperator(scanner.next().charAt(0));
+        } catch (IllegalArgumentException e) {
+            String message = e.getMessage();
+            System.out.println(message + " 다시 입력해주세요.");
+        }
+    }
+
+    // 계산 메서드 (Generic 활용)
     public double calculate(T num1, T num2, OperatorType op) {
-        double tmp = 0.0;
         double n1 = num1.doubleValue();
         double n2 = num2.doubleValue();
         switch (op) {
             case ADD:
-                tmp = n1 + n2;
-                break;
+                return n1 + n2;
             case SUBTRACT:
-                tmp = n1 - n2;
-                break;
+                return n1 - n2;
             case MULTIPLY:
-                tmp = n1 * n2;
-                break;
+                return n1 * n2;
             case DIVIDE:
                 if (n2 == 0) {
                     try {
@@ -32,13 +52,49 @@ public class ArithmeticCalculator<T extends Number> {
                         break;
                     }
                 }
-                tmp = n1 / n2;
+                return n1 / n2;
+            default:
+
         }
-        return tmp;
+        return 0;
     }
 
     public void addResult(Double result) {
+        System.out.println("계산 결과: " + result);
         results.add(result);
+    }
+
+    public void optionPrint() {
+        System.out.println("==================== 목록 ====================");
+        System.out.println("1. 가장 오래된 계산 기록 삭제");
+        System.out.println("2. 저장된 연산 결과들 중 입력받은 값보다 큰 결과값들을 출력");
+        System.out.println("3. 계산한 결과값 목록 조회");
+        System.out.println("4. 계속 계산하기");
+        System.out.println("--------------------------------------------");
+        System.out.print("원하는 항목을 입력해주세요. (exit 입력 시 계산 종료): ");
+        switch (scanner.next()) {
+            case "1":
+                removeResult();
+                optionPrint();
+                break;
+            case "2":
+                List<Double> resultGreaterThan = getResultGreaterThan(getNum1(), getNum2());
+                System.out.println("입력받은 값보다 큰 결과값 목록: " + resultGreaterThan);
+                break;
+            case "3":
+                System.out.println("현재 저장된 결과: " + getResults());
+                optionPrint();
+                break;
+            case "4":
+                System.out.println("계속 계산을 진행합니다.");
+                break;
+            case "exit":
+                System.out.println("계산을 종료합니다.");
+                return;
+            default:
+                System.out.println("잘못된 입력 값입니다. 다시 입력해주세요.");
+                optionPrint();
+        }
     }
 
     public void removeResult() {
@@ -50,6 +106,18 @@ public class ArithmeticCalculator<T extends Number> {
         return results.stream()
                 .filter(result -> result > num1 && result > num2)
                 .collect(Collectors.toList());
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public OperatorType getOp() {
+        return op;
     }
 
     public List<Double> getResults() {
